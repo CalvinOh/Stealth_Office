8ba661905857c1e6133fd77a121aa91ab7187252
CalvinOh <calvinohwyekit@gmail.com>
2019-04-14 17:24:22 -0500

    NavMesh Working

--- StealthOffice/Assets/Scripts/NavMesh/GuardMove.cs
+++ StealthOffice/Assets/Scripts/NavMesh/GuardMove.cs
@@ -6,14 +6,45 @@ using UnityEngine.AI;
 
 public class GuardMove : MonoBehaviour
 {
+    public enum States
+    {
+        Chase,
+        Patrol,
+        Catch,
+    }
+
+
     [SerializeField]
-    Transform _destination;
+    Transform target;
+    [SerializeField]
+    LayerMask layerMask;
+    [SerializeField]
+    Transform center;
+    [SerializeField]
+    float radius;
+    [SerializeField]
+    States curState;
 
     NavMeshAgent _navMeshAgent;
+    public Collider[] sphereCollider;
+
+    [SerializeField]
+    bool _patrolWaiting;
+    [SerializeField]
+    float _totalWaitTime = 3.0f;
+
+    ConnectedWaypoint _currentWaypoint;
+    ConnectedWaypoint _previousWaypoint;
+
+    bool _travelling;
+    bool _waiting;
+    float _waitTimer;
+    int _waypointsVisited;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
 
         if(_navMeshAgent == null)
@@ -22,18 +53,140 @@ public class GuardMove : MonoBehaviour
         }
         else
         {
-            SetDestination();
+            if (_currentWaypoint == null)
+            {
+                GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+                if (allWaypoints.Length > 0)
+                {
+                    while (_currentWaypoint == null)
+                    {
+                        int random = UnityEngine.Random.Range(0, allWaypoints.Length);
+                        ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>();
+
+                        if (startingWaypoint != null)
+                        {
+                            _currentWaypoint = startingWaypoint;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("Failed to find waypoint");
+            }
         }
+        curState = States.Patrol;
+        Patrolling();
     }
 
-    private void SetDestination()
+    bool IsPlayerWithinHearing()
     {
-        if(_destination != null)
+        sphereCollider = Physics.OverlapSphere(center.position, radius, layerMask);
+        if (sphereCollider.Length != 0)
         {
-            Vector3 targetVector = _destination.transform.position;
-            _navMeshAgent.SetDestination(targetVector);
+            return true;
         }
+       
+        return false;
     }
 
+    private void Update()
+    {
+        StatesUpdate();
+       
+    }
+    void StatesUpdate()
+    {
+        switch (curState)
+        {
+            case States.Patrol: UpdatePatrolState(); break;
+            case States.Chase: UpdateChaseState(); break;
+            case States.Catch: UpdateCatchState(); break;
+        }
 
+    }
+
+    private void UpdateCatchState()
+    {
+        Debug.Log("Caught, Game Over");
+    }
+
+    private void UpdateChaseState()
+    {
+        if (IsPlayerWithinHearing())
+        {
+            if (target != null)
+            {
+                _navMeshAgent.speed = 5.0f;
+                Vector3 targetVector = target.transform.position;
+                _navMeshAgent.SetDestination(targetVector);
+            }
+        }
+        else
+            curState = States.Patrol;
+    }
+
+    private void UpdatePatrolState()
+    {
+        _navMeshAgent.speed = 3.5f;
+        if (_travelling && _navMeshAgent.remainingDistance < 1.0f)
+        {
+            _travelling = false;
+            _waypointsVisited++;
+
+            if (_patrolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 0f;
+            }
+            else
+            {
+                Patrolling();
+            }
+        }
+
+        if (_waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            if (_waitTimer >= _totalWaitTime)
+            {
+                _waiting = false;
+
+                Patrolling();
+            }
+        }
+        if (IsPlayerWithinHearing())
+            curState = States.Chase;
+    }
+
+    private void Patrolling()
+    {
+        if (_waypointsVisited > 0)
+        {
+            ConnectedWaypoint nextWaypoint = _currentWaypoint.NextWaypoint(_previousWaypoint);
+            _previousWaypoint = _currentWaypoint;
+            _currentWaypoint = nextWaypoint;
+        }
+
+        Vector3 targetVector = _currentWaypoint.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+        _travelling = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(center.position, radius);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            curState = States.Catch;
+        }
+        else
+            curState = States.Chase;
+    }
 }
